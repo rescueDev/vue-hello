544b5bca9a462a62c8d5d59928f70b4dc96b6fd4
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-16 15:00:01 +0100

    es con bonus finito

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@
 
 var app = new Vue({
   el: "#root",
+  data: {
+    message: "Hello Vue!!",
+    // Bonus: Aggiungere un’immagine presa anch’essa da un data
+    image: "https://italiancoders.it/wp-content/uploads/2018/01/vue.png",
+    //Bonus : alternative text
+    altText: "Vue background",
+    //Bonus: classi
+    mieClassi: "hide",
+  },
+  // Altro possibile Bonus:
+  // gestire una funzione, al click faccio apparire o sparire il contenuto della pagina
+  methods: {
+    genFunction: function () {
+      if (this.mieClassi === "hide") {
+        this.mieClassi = "show";
+      } else {
+        this.mieClassi = "hide";
+      }
+    },
+  },
 });
-
-// Bonus: Aggiungere un’immagine presa anch’essa da un data
-// Altro possibile Bonus:
-// gestire una funzione con un evento nel modo Vue.
