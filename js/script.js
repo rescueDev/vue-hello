bd5c8f76cddd3c282e8e32b2278e73bd6a3b3a39
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-16 14:16:05 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue({
+  el: "#root",
+});
+
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+// Altro possibile Bonus:
+// gestire una funzione con un evento nel modo Vue.
